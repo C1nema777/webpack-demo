cd17572b556ee9eb508795b6ac83041f80ee5992
C1nema777 <17681881408@163.com>
2021-06-18 18:12:21 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
-  element.innerHTML = _.join(['Hello', 'webpack']);
+  // lodash 是由当前 script 脚本 import 导入进来的
+  element.innerHTML = _.join(["Hello", "webpack"]);
+  element.classList.add("hello");
 
-  return element
+  // 添加图像
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
 }
 
 document.body.appendChild(component());
